6e3a3ee83acfef21be2eeecb5b63865beb68ef9c
Tushar Mishra <tushar.mishra@objectedge.com>
2023-03-21 18:50:32 +0530

    updated allocated_qty to zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from connector import engine
 from sqlalchemy import text
+from sqlalchemy import Table
 from sqlalchemy.orm import Session
 import logging
 
@@ -28,48 +29,62 @@ def execute_query(query, params=None, fetch=False):
     return result
 
 
-def create_table(name, columns):
-    try:
-        columns = ", ".join(f"{col} {typ}" for col, typ in columns.items())
-        stmt = text(f"CREATE TABLE IF NOT EXISTS {name} ({columns})")
-        return execute_query(stmt)
-    except Exception as e:
-        print(f"create_table failed due to {e}")
+#
+#
+# def create_table(name, columns):
+#     try:
+#         columns = ", ".join(f"{col} {typ}" for col, typ in columns.items())
+#         stmt = text(f"CREATE TABLE IF NOT EXISTS {name} ({columns})")
+#         return execute_query(stmt)
+#     except Exception as e:
+#         print(f"create_table failed due to {e}")
+#
+#
+# customer_partner_relation_table = {
+#     "customer_status": "text",
+#     "partner_status": "text",
+#     "cpqmodel": "text",
+#     "customer_account_type": "text",
+#     "customer_sfdc_account_id": "text",
+#     "customer_woc_ref": "bigint",
+#     "package": "text",
+#     "partner_sfdc_account_id": "text",
+#     "total_qty": "int",
+#     "subscription_lines": "jsonb",
+#     "entities": "jsonb",
+# }
+# create_table("customer_partner_relation_table", customer_partner_relation_table)
+#
+# subscription_table = {
+#     "id": "serial",
+#     "qty": "int",
+#     "cpqmodel": "text",
+#     "customer_sfdc_account_id": "text",
+#     "allocated_qty": "int",
+#     "subscription_line_id": "bigint",
+# }
+# create_table("subscription_table", subscription_table)
+#
+#
+# def import_csv_data(table, file, delimiter):
+#     stmt = text(f"COPY {table} FROM '{file}' DELIMITER '{delimiter}' CSV HEADER")
+#     return execute_query(stmt)
+#
+#
+# import_csv_data(
+#     "customer_partner_relation_table(customer_status,partner_status,cpqmodel,customer_account_type,customer_sfdc_account_id,customer_woc_ref,package,partner_sfdc_account_id,total_qty,subscription_lines)",
+#     "D:/Python Assignments/assignment-2/output.csv",
+#     "|",
+# )
 
 
-customer_partner_relation_table = {
-    "customer_status": "text",
-    "partner_status": "text",
-    "cpqmodel": "text",
-    "customer_account_type": "text",
-    "customer_sfdc_account_id": "text",
-    "customer_woc_ref": "bigint",
-    "package": "text",
-    "partner_sfdc_account_id": "text",
-    "total_qty": "int",
-    "subscription_lines": "jsonb",
-    "entities": "jsonb",
-}
-create_table("customer_partner_relation_table", customer_partner_relation_table)
-
-subscription_table = {
-    "id": "serial",
-    "qty": "int",
-    "cpqmodel": "text",
-    "customer_sfdc_account_id": "text",
-    "allocated_qty": "int",
-    "subscription_line_id": "bigint",
-}
-create_table("subscription_table", subscription_table)
-
-
-def import_csv_data(table, file, delimiter):
-    stmt = text(f"COPY {table} FROM '{file}' DELIMITER '{delimiter}' CSV HEADER")
+def set_to_zero():
+    stmt = text(
+        """UPDATE customer_partner_relation_table SET subscription_lines = 
+        ( SELECT jsonb_agg( jsonb_set(element, '{allocated_qty}', '0', false) ) 
+        FROM jsonb_array_elements(subscription_lines) AS element );"""
+    )
     return execute_query(stmt)
 
 
-import_csv_data(
-    "customer_partner_relation_table(customer_status,partner_status,cpqmodel,customer_account_type,customer_sfdc_account_id,customer_woc_ref,package,partner_sfdc_account_id,total_qty,subscription_lines)",
-    "D:/Python Assignments/assignment-2/output.csv",
-    "|",
-)
+set_to_zero()
